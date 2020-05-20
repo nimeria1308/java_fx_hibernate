0c69e451de4655a6ba47fbe30704cc638a1e6b1e
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-21 01:18:53 +0300

    Add entities to config

--- src/simonadimitrova/electricitycompany/model/configuration/HibernateUtil.java
+++ src/simonadimitrova/electricitycompany/model/configuration/HibernateUtil.java
@@ -9,6 +9,7 @@ import org.hibernate.HibernateError;
 import org.hibernate.Session;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
+import simonadimitrova.electricitycompany.model.entities.*;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -24,8 +25,14 @@ public class HibernateUtil {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml)
             // config file.
-            AnnotationConfiguration configuration = new AnnotationConfiguration();
-            configuration.configure(HibernateUtil.class.getResource("hibernate.cfg.xml"));
+            AnnotationConfiguration configuration = new AnnotationConfiguration()
+                    .addAnnotatedClass(Client.class)
+                    .addAnnotatedClass(ClientMeasurement.class)
+                    .addAnnotatedClass(ClientPrice.class)
+                    .addAnnotatedClass(Employee.class)
+                    .addAnnotatedClass(Facility.class)
+                    .addAnnotatedClass(FacilityExpense.class)
+                    .configure(HibernateUtil.class.getResource("hibernate.cfg.xml"));
             sessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
             // Log the exception.
