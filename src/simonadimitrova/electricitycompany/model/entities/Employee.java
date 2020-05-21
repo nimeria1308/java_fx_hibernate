85d4225ca8d21a19c45d56bf5e3c868e606a4a89
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-21 23:56:04 +0300

    extended employee entity

--- src/simonadimitrova/electricitycompany/model/entities/Employee.java
+++ src/simonadimitrova/electricitycompany/model/entities/Employee.java
@@ -32,4 +32,37 @@ public class Employee {
     @Digits(integer = 5, fraction = 3, message = "price value must be of the format XXXXX.YYY")
     @Column(name = "salary", nullable = false)
     private BigDecimal salary; // in BGN
+
+    public Employee() {
+    }
+
+    public Employee(long id, String name, BigDecimal salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
 }
