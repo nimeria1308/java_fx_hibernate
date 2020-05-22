3626c9726dc9729f809f431d424a220e584271b1
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-22 05:37:24 +0300

    It now reads Employees

--- src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
+++ src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
@@ -1,4 +1,35 @@
 package simonadimitrova.electricitycompany.app.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import simonadimitrova.electricitycompany.model.dao.EmployeeDAO;
+import simonadimitrova.electricitycompany.model.entities.Employee;
+
+import java.math.BigDecimal;
+
 public class EmployeesController {
+    @FXML
+    private TableView table;
+
+    @FXML
+    private TableColumn idColumn;
+
+    @FXML
+    private TableColumn nameColumn;
+
+    @FXML
+    private TableColumn salaryColumn;
+
+    @FXML
+    private void initialize() {
+        ObservableList<Employee> employees = FXCollections.observableArrayList(EmployeeDAO.getAll());
+        idColumn.setCellValueFactory(new PropertyValueFactory<Employee, Long>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
+        salaryColumn.setCellValueFactory(new PropertyValueFactory<Employee, BigDecimal>("salary"));
+        table.setItems(employees);
+    }
 }
