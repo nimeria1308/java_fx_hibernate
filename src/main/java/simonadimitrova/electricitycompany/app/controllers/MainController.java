8f30e546f5ea8a31501122980fefd2e6dc533fbe
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-22 05:18:56 +0300

    Added about

--- src/main/java/simonadimitrova/electricitycompany/app/controllers/MainController.java
+++ src/main/java/simonadimitrova/electricitycompany/app/controllers/MainController.java
@@ -3,10 +3,16 @@ package simonadimitrova.electricitycompany.app.controllers;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 public class MainController {
     @FXML
     public void onExit(ActionEvent event) {
         Platform.exit();
+    }
+
+    @FXML
+    public void onAbout(ActionEvent event) {
+        new Alert(Alert.AlertType.INFORMATION, "Copyright (c) Simona Dimitrova").showAndWait();
     }
 }
