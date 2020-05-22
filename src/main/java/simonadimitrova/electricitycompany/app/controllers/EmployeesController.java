08108ba016f6cf3289308b60564c6ecd5e364d55
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-22 06:25:36 +0300

    can now edit employees

--- src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
+++ src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
@@ -3,6 +3,7 @@ package simonadimitrova.electricitycompany.app.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -11,6 +12,9 @@ import javafx.util.converter.BigDecimalStringConverter;
 import simonadimitrova.electricitycompany.model.dao.EmployeeDAO;
 import simonadimitrova.electricitycompany.model.entities.Employee;
 
+import javax.persistence.Table;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.math.BigDecimal;
 
 public class EmployeesController {
@@ -39,5 +43,47 @@ public class EmployeesController {
         salaryColumn.setCellValueFactory(new PropertyValueFactory<Employee, Double>("salary"));
         salaryColumn.setCellFactory(TextFieldTableCell.<Employee, BigDecimal>forTableColumn(new BigDecimalStringConverter()));
         table.setItems(employees);
+    }
+
+    @FXML
+    private void onEditName(TableColumn.CellEditEvent<Employee, String> event) {
+        TableView.TableViewSelectionModel<Employee> selectionModel = table.getSelectionModel();
+        Employee employee = selectionModel.getSelectedItem();
+        String oldName = employee.getName();
+        employee.setName(event.getNewValue());
+        try {
+            EmployeeDAO.saveOrUpdate(employee);
+        } catch (ConstraintViolationException ex) {
+            String message = "";
+            for (ConstraintViolation v: ex.getConstraintViolations()) {
+                message += v.getMessage();
+                message += '\n';
+            }
+
+            new Alert(Alert.AlertType.ERROR, message).showAndWait();
+            employee.setName(oldName);
+            table.getItems().set(selectionModel.getSelectedIndex(), employee);
+        }
+    }
+
+    @FXML
+    private void onEditSalary(TableColumn.CellEditEvent<Employee, BigDecimal> event) {
+        TableView.TableViewSelectionModel<Employee> selectionModel = table.getSelectionModel();
+        Employee employee = selectionModel.getSelectedItem();
+        BigDecimal oldSalary = employee.getSalary();
+        employee.setSalary(event.getNewValue());
+        try {
+            EmployeeDAO.saveOrUpdate(employee);
+        } catch (ConstraintViolationException ex) {
+            String message = "";
+            for (ConstraintViolation v: ex.getConstraintViolations()) {
+                message += v.getMessage();
+                message += '\n';
+            }
+
+            new Alert(Alert.AlertType.ERROR, message).showAndWait();
+            employee.setSalary(oldSalary);
+            table.getItems().set(selectionModel.getSelectedIndex(), employee);
+        }
     }
 }
