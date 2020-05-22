0f89c29aa86ed8f1aa3feb236fd0563f5f9eaa96
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-22 05:13:45 +0300

    onExit menu

--- src/main/java/simonadimitrova/electricitycompany/app/controllers/MainController.java
+++ src/main/java/simonadimitrova/electricitycompany/app/controllers/MainController.java
@@ -1,4 +1,12 @@
 package simonadimitrova.electricitycompany.app.controllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
+    @FXML
+    public void onExit(ActionEvent event) {
+        Platform.exit();
+    }
 }
