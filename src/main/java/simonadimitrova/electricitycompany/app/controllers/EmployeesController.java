707fc5a6933ce5eb6064fe562c643fe4d41b714a
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-22 05:55:38 +0300

    Employee stuff is now editable

--- src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
+++ src/main/java/simonadimitrova/electricitycompany/app/controllers/EmployeesController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.BigDecimalStringConverter;
 import simonadimitrova.electricitycompany.model.dao.EmployeeDAO;
 import simonadimitrova.electricitycompany.model.entities.Employee;
 
@@ -26,10 +28,16 @@ public class EmployeesController {
 
     @FXML
     private void initialize() {
+        loadTable();
+    }
+
+    private void loadTable() {
         ObservableList<Employee> employees = FXCollections.observableArrayList(EmployeeDAO.getAll());
         idColumn.setCellValueFactory(new PropertyValueFactory<Employee, Long>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
-        salaryColumn.setCellValueFactory(new PropertyValueFactory<Employee, BigDecimal>("salary"));
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        salaryColumn.setCellValueFactory(new PropertyValueFactory<Employee, Double>("salary"));
+        salaryColumn.setCellFactory(TextFieldTableCell.<Employee, BigDecimal>forTableColumn(new BigDecimalStringConverter()));
         table.setItems(employees);
     }
 }
